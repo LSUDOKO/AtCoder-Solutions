56edce830733d7687fd4898e95e3e6446d6b62d3
LSUDOKO <adoranto737@gmail.com>
2025-09-28 02:52:03 +0530

    Refactor contest solutions for improved readability and efficiency

--- contest/contest1/2.cpp
+++ contest/contest1/2.cpp
@@ -1,48 +1,94 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cin >> n;
+//     vector<int> ans(n);
+//     map<int,int> frq;
+//     vector<int> rem;
+//     vector<int> pu;
+//     bool check = true;
+
+//     for (int i = 0; i < n; i++) {
+//         cin >> ans[i];
+//         frq[ans[i]]++;
+//         if (ans[i] != -1) {
+//             pu.push_back(ans[i]);
+//         }
+//     }
+
+//     for (int i = 1; i <= n; i++) {
+//         if (find(pu.begin(), pu.end(), i) == pu.end()) {
+//             rem.push_back(i);
+//         }
+//     }
+
+//     for (auto p : frq) {
+//         if (p.first != -1 && p.second > 1) {
+//             check = false;
+//         }
+//     }
+
+//     if (check) {
+//         int j = 0;
+//         for (int i = 0; i < n; i++) {
+//             if (ans[i] == -1) {
+//                 ans[i] = rem[j++];
+//             }
+//         }
+//         cout << "Yes"<<endl;
+//         for (int i = 0; i < n; i++) {
+//             cout << ans[i] << " ";
+//         }
+//         cout <<endl;
+//     } else {
+//         cout << "No"<<endl;
+//     }
+// }
+
+
+// optimized sol
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int main() {
-    int n;
-    cin >> n;
-    vector<int> ans(n);
-    map<int,int> frq;
-    vector<int> rem;
-    vector<int> pu;
-    bool check = true;
-
-    for (int i = 0; i < n; i++) {
-        cin >> ans[i];
-        frq[ans[i]]++;
-        if (ans[i] != -1) {
-            pu.push_back(ans[i]);
-        }
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    for(int i = 0; i < N; i++) {
+        cin >> A[i];
     }
-
-    for (int i = 1; i <= n; i++) {
-        if (find(pu.begin(), pu.end(), i) == pu.end()) {
-            rem.push_back(i);
-        }
-    }
-
-    for (auto p : frq) {
-        if (p.first != -1 && p.second > 1) {
-            check = false;
-        }
-    }
-
-    if (check) {
-        int j = 0;
-        for (int i = 0; i < n; i++) {
-            if (ans[i] == -1) {
-                ans[i] = rem[j++];
+    set<int> used;
+    for(int i = 0; i < N; i++) {
+        if(A[i] != -1) {
+            if(used.count(A[i])) {
+                cout << "No" << endl;
+                return 0;
             }
+            used.insert(A[i]);
         }
-        cout << "Yes"<<endl;
-        for (int i = 0; i < n; i++) {
-            cout << ans[i] << " ";
-        }
-        cout <<endl;
-    } else {
-        cout << "No"<<endl;
     }
+    vector<int> remaining;
+    for(int x = 1; x <= N; x++) {
+        if(!used.count(x)) {
+            remaining.push_back(x);
+        }
+    }
+    vector<int> P = A;
+    int j = 0;
+    for(int i = 0; i < N; i++) {
+        if(P[i] == -1) {
+            P[i] = remaining[j];
+            j++;
+        }
+    }
+    cout << "Yes" << endl;
+    for(int i = 0; i < N; i++) {
+        if(i > 0) cout << " ";
+        cout << P[i];
+    }
+    cout << endl;
+    return 0;
 }
--- contest/contest1/3.cpp
+++ contest/contest1/3.cpp
@@ -29,6 +29,9 @@
 //     }
 // }
 
+
+// optimized code
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -71,4 +74,46 @@ int main() {
             cout << ans << endl;
         }
     }
+}
+
+//another way 
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int N, Q;
+    cin >> N >> Q;
+    vector<long long> A(N + 1);
+    for (int i = 1; i <= N; i++) cin >> A[i];
+    vector<long long> prefix(N + 1, 0);
+    for (int i = 1; i <= N; i++) prefix[i] = prefix[i - 1] + A[i];
+    long long S = 0;
+    long long mod = N;
+    for (int q = 0; q < Q; q++) {
+        int type;
+        cin >> type;
+        if (type == 1) {
+            long long c;
+            cin >> c;
+            S = (S + c) % mod;
+        } else {
+            long long l, r;
+            cin >> l >> r;
+            long long K = r - l + 1;
+            long long sp = (S + l - 1) % mod + 1;
+            long long ep = sp + K - 1;
+            long long sum = 0;
+            if (ep <= N) {
+                sum = prefix[ep] - prefix[sp - 1];
+            } else {
+                sum = prefix[N] - prefix[sp - 1] + prefix[ep - N];
+            }
+            cout << sum << '\n';
+        }
+    }
+    return 0;
 }
